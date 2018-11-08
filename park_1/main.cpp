64e694006fbce1903145d306fc4cdc0a33c166f0
parkjiyong94122 <42165663+parkjiyong94122@users.noreply.github.com>
2018-11-08 12:20:49 +0900

    .

--- park_1/main.cpp
+++ park_1/main.cpp
@@ -5,10 +5,41 @@ using namespace std;
 
 int main(void)
 {
-	int num = 20;
-	
-	cout << "hello world!!~" << endl;
+	char name[100];
+	char lang[200];
 
+	cout << "이름을 입력해주세요 :  ";
+	cin >> name;
+
+	cout << "좋아하는 언어 입력 :  ";
+	cin >> lang;
+
+	cout << name << lang << endl;
+
+
+	/*&
+	int val;
+
+	cout << "1번째 숫자 입력 :  ";
+	cin >> val;
+	
+	int val2;
+
+	cout << "2번째 숫자 입력 :  ";
+	cin >> val2;
+
+	int result = val + val2;
+
+	cout << result;
+	*/
+	/*
+	int num = 20;
+	cout << "hello world!!~" << endl;
+	cout << "hello " << "world " << endl;
+	cout << num << ' ' << 'A';
+	cout << ' ' << 3.14 << endl;
+	*/
+	
 
 	return 0;
 }
