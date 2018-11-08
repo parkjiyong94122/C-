17c06537053cce2b7264355a45fc89ff38a67036
parkjiyong94122 <42165663+parkjiyong94122@users.noreply.github.com>
2018-11-08 12:43:26 +0900

    ..

--- park_1/main.cpp
+++ park_1/main.cpp
@@ -1,10 +1,50 @@
 #include <iostream>
-
-
 using namespace std;
+
+
+namespace BestComImpl {
+	void SimpleFunc(void);
+}
+namespace ProgComImpl {
+	void SimpleFunc(void);
+}
+
+int adder(int num1 = 1, int num2 = 2);
+
+
+void MyFunc(void)
+{
+	cout << "MyFunc(void) Called " << endl;
+}
+void MyFunc(char c)
+{
+	cout << "MyFunc(char c) Called " << endl;
+}
+void MyFunc(int a , int b)
+{
+	cout << "MyFunc(int a, int b) Called " << endl;
+}
 
 int main(void)
 {
+
+	/* name space
+	BestComImpl::SimpleFunc();
+	ProgComImpl::SimpleFunc();
+	*/
+
+
+	/*default 매개변수
+	cout << "1+2" << adder();
+	cout << "2+3" << adder(2, 3);
+	*/
+
+	/* 함수 오버로딩
+	MyFunc();
+	MyFunc('a');
+	MyFunc(2,1);
+	*/
+	/*
 	char name[100];
 	char lang[200];
 
@@ -15,9 +55,9 @@ int main(void)
 	cin >> lang;
 
 	cout << name << lang << endl;
+	*/
 
-
-	/*&
+	/*
 	int val;
 
 	cout << "1번째 숫자 입력 :  ";
@@ -42,4 +82,19 @@ int main(void)
 	
 
 	return 0;
+}
+
+void BestComImpl::SimpleFunc(void)
+{
+	cout << "BestCom의 함수." << endl;
+}
+
+void ProgComImpl::SimpleFunc(void)
+{
+	cout << "ProgCom의 함수." << endl;
+}
+
+int adder(int num1, int num2)
+{
+	return num1 + num2;
 }
