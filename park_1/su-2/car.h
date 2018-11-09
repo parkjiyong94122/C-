e4c04b084c2fff651feb1a7e40a8ecd218f03a99
parkjiyong94122 <42165663+parkjiyong94122@users.noreply.github.com>
2018-11-09 14:38:22 +0900

    .

--- park_1/su-2/car.h
+++ park_1/su-2/car.h
@@ -1,6 +1,7 @@
+#ifndef _CAR_H_
+#define _CAR_H_
 #include <iostream>
 #include <cstring>
-
 namespace CAR_CONST
 {
 	//const int ID_LEN = 20, MAX_SPD = 200, FUEL_STEP = 20, ACC_STEP = 10, BRK_STEP = 10;
@@ -27,3 +28,5 @@ public:
 	void Accel();
 	void Break();
 };
+
+#endif
