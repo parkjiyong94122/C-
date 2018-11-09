d0f65f168ee12a03d7528b533fdc02c32e3f7b0c
parkjiyong94122 <42165663+parkjiyong94122@users.noreply.github.com>
2018-11-09 14:12:10 +0900

    .

--- park_1/su-2/20181109.cpp
+++ park_1/su-2/20181109.cpp
@@ -1,6 +1,95 @@
-#include <iostream>
+#include"car.h"
 
 using namespace std;
+
+// 자동차 프로그램(소유자, 연료량, 현재속도)
+
+// 어느 구조체에 들어가는 가독성을 위해
+namespace CAR_CONST
+{
+	const int ID_LEN = 20, MAX_SPD = 200, FUEL_STEP = 20, ACC_STEP = 10,BRK_STEP = 10;
+	/*
+	enum
+	{
+		ID_LEN = 20,
+		MAX_SPD = 200,
+		FUEL_STEP = 20,
+		ACC_STEP = 10,
+		BRK_STEP = 10,
+	};
+	*/
+}
+/*
+#define ID_LEN 20
+#define MAX_SPD 200
+#define FUEL_STEP 20
+#define ACC_STEP 10
+#define BRK_STEP 10
+*/
+
+/*
+	car 구조체를 class로 변경 및 적용,
+	- 캡슐화 (정보은닉, 법위설정)
+	- 정보은닉을 위한 접근제어지시자
+		public 어디서든 접근가능
+		private 클래스 내에서만 접근가능
+		protected 상속관계 내에서 접근가능
+	- 범위설정
+		개발자가 분석후 정의하기 나름.
+		
+*/
+
+//class 캡슐화(정보은닉, 범위) 상속 추상화 다
+class Car
+{
+
+private:	
+	char gamerID[CAR_CONST::ID_LEN];
+	int fuelGauge;
+	int curSpeed;
+
+public:
+	void InitMembers(const char* id ,int fuel);
+	void ShowCarState();
+	void Accel();
+	void Break();
+};
+void Car::InitMembers(const char* id, int fuel)
+{
+	strcpy(gamerID, id);
+	fuelGauge = fuel;
+	curSpeed = 0;
+}
+void Car::ShowCarState()
+{
+	cout << "소유자 :" << gamerID << endl;
+	cout << "연료량 :" << fuelGauge << endl;
+	cout << "속  도 :" << curSpeed << endl;
+}
+void Car::Accel()
+{
+	//연료량 체크
+	if (fuelGauge <= 0)
+		return;
+	else
+		fuelGauge -= CAR_CONST::FUEL_STEP;
+	//최고 속도 체크
+	if (curSpeed + CAR_CONST::ACC_STEP >= CAR_CONST::MAX_SPD)
+	{
+		curSpeed = CAR_CONST::MAX_SPD;
+		return;
+	}
+	curSpeed += CAR_CONST::ACC_STEP;
+}
+void Car::Break()
+{
+	if (curSpeed - CAR_CONST::BRK_STEP < 0)
+	{
+		curSpeed = 0;
+		return;
+	}
+	curSpeed -= CAR_CONST::BRK_STEP;
+}
 
 void SwapByRef(int &ref1, int &ref2)
 {
@@ -17,8 +106,24 @@ int & RefRetFuncOne(int &ref)
 
 int main(void)
 {
+	//car 구조체 사용을 위한 초기화,
+
+	Car run77;
+	run77.InitMembers("run77", 100);
+	run77.Accel();
+	run77.Accel();
+	run77.ShowCarState();
+	run77.Break();
+	run77.ShowCarState();
+
+	Car run88;
+	run88.InitMembers("run88", 100);
+	run88.Accel();
+	run88.Break();
+	run88.ShowCarState();
+
 	//반환형이 참조자인 경우,
-	
+	/*
 	int num = 1;
 
 	int &num2 = RefRetFuncOne(num);
@@ -27,7 +132,7 @@ int main(void)
 	num2++;
 	cout << "num : " << num << endl;
 	cout << "num2 : " << num2 << endl;
-
+	*/
 
 	//참조자를 이용한 call by reference
 	/*
---
+++ park_1/su-2/car.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+
+namespace CAR_CONST
+{
+	const int ID_LEN = 20, MAX_SPD = 200, FUEL_STEP = 20, ACC_STEP = 10, BRK_STEP = 10;
+	/*
+	enum
+	{
+	ID_LEN = 20,
+	MAX_SPD = 200,
+	FUEL_STEP = 20,
+	ACC_STEP = 10,
+	BRK_STEP = 10,
+	};
+	*/
+}
