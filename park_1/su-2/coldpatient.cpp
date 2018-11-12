6b082f0bd05c90490df41f2808b0064b6fde8203
parkjiyong94122 <42165663+parkjiyong94122@users.noreply.github.com>
2018-11-12 17:44:10 +0900

    .

--- park_1/su-2/coldpatient.cpp
+++ park_1/su-2/coldpatient.cpp
@@ -1,8 +1,6 @@
-
 #include"coldpatient.h"
 
 using namespace std;
-
 
 //콧물 캡슐
 void SinivelCap::Take()
@@ -11,19 +9,16 @@ void SinivelCap::Take()
 }
 
 //재채기 캡슐
-
 void  SneezeCap::Take()
 {
 	cout << "재채기가 멎습니다." << endl;
 }
 
 //코막힘 캡슐
-
 void SnuffleCap::Take()
 {		
 	cout << "코가 뚫립니다." << endl;
 }
-
 
 void AllCap::Take()
 {	
@@ -32,27 +27,25 @@ void AllCap::Take()
 	snu.Take();
 }
 
-
-
-	//감기 증상,환자 특징, 감기 세기
-
-	//초기화 ,상태 출력,약 처방, 약 섭취
-
-	//콧물약 복용
+//콧물약 복용
 void ColdPatient::TakeSinivelCap(SinivelCap &cap)
 {
 	cap.Take();
 }
+
 //재채기약 복용
 void ColdPatient::TakeSneezeCap(SneezeCap &cap)
 {
 	cap.Take();
 }
+
 //코막힘약 복용
 void ColdPatient::TakeSnuffleCap(SnuffleCap &cap)
 {
 	cap.Take();
 }
+
+//모든약 복용
 void ColdPatient::TakeCap(AllCap &cap)
 {
 	cap.Take();
