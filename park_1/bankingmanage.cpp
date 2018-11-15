7f5150c53af13aaaad9e93b5fcd61347e62d1fe5
parkjiyong94122 <42165663+parkjiyong94122@users.noreply.github.com>
2018-11-15 16:55:28 +0900

    .

--- park_1/bankingmanage.cpp
+++ park_1/bankingmanage.cpp
@@ -83,14 +83,3 @@ int main(void)
 	return 0;
 }
 
-void call_menu()
-{
-	cout << "---------Menu---------" << endl;
-	cout << "1. 계좌 개설 " << endl;
-	cout << "2. 입금" << endl;
-	cout << "3. 출금" << endl;
-	cout << "4. 계좌정보 전체 출력" << endl;
-	cout << "5. 프로그램 종료" << endl << endl;
-
-	cout << "선택 : ";
-}
