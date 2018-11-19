f4c826ef16313507d9afc1c7dd9e3a803beb0fcc
parkjiyong94122 <42165663+parkjiyong94122@users.noreply.github.com>
2018-11-19 17:22:16 +0900

    .

--- park_1/su-2/main.cpp
+++ park_1/su-2/main.cpp
@@ -3,7 +3,77 @@
 
 using namespace std;
 
+//연산자 오버로딩
+class Point
+{
+private:
+	int xpos, ypos;
+public:
+	Point(int x = 0, int y = 0) :xpos(x), ypos(y)
+	{}
+	void ShowPoistion() const
+	{
+		cout << '[' << xpos <<". " <<ypos<< ']' << endl;
+	}
+
+	//멤버함수에 의한 연산자 오버로딩	*이항연산
+	/*
+	Point operator+(const Point &ref) const
+	{
+		Point pos(xpos + ref.xpos, ypos + ref.ypos);
+		return pos;
+	}
+	*/
+	/*
+	Point & operator--(int)		//전위 단항 연산
+	{
+		xpos--;
+		ypos--;
+		return *this;
+	}
+	*/
+	Point operator--(int)
+	{
+		const Point retobj(xpos, ypos);
+		xpos += 1;
+		ypos += 1;
+		return retobj;
+	}
+	//전역함수에 의한 연산자 오버로딩
+	Point operator*(int times)
+	{
+		Point pos(xpos*times, ypos*times);
+		return pos;
+	}
+
+	friend Point operator+(const Point &pos1,const Point &pos2);	//이항 연산
+	friend Point & operator++(Point &pos);						//단항 연산
+	friend Point operator++(Point &pos,int);					//단항 연산
+};
+/*전위 단항 연산
+Point & operator++(Point &pos)
+{
+	pos.xpos++;
+	pos.ypos++;
+	return pos;
+}
+*/
+Point operator++(Point &pos, int)
+{
+	const Point retobj(pos.xpos, pos.ypos);
+	pos.xpos += 1;
+	pos.ypos += 1;
+	return retobj;
+}
+
+Point operator+(const Point &pos1, const Point &pos2)
+{
+	Point pos(pos1.xpos + pos2.xpos, pos1.ypos + pos2.ypos);
+	return pos;
+}
+
 // mutable 쓸일이 거의 없음.
+/*
 class SoSimple
 {
 private:
@@ -21,6 +91,7 @@ public:
 		num2 = num1; // const 멤버 변수 변경 가능.
 	}
 };
+*/
 
 //static 함수 (클래스 함수)
 /*
@@ -39,6 +110,7 @@ public:
 };
 int SoSimple::num2;
 */
+
 //const static 변수 (클래스 변수)
 class CountryArea
 {
@@ -46,6 +118,7 @@ public:
 	const static int RUSSIA = 8976;
 	const static int SOUTH_KOREA = 7877;
 };
+
 //static 변수 (클래스 변수)
 /*
 //int simObjCnt = 0;
@@ -77,6 +150,7 @@ public:
 };
 int SoComplx::cmxObjCnt;
 */
+
 //정적 지역 변수
 /*
 void Counter()
@@ -87,6 +161,7 @@ void Counter()
 
 }
 */
+
 /*friend 선언 class
 class Girl;
 class boy
@@ -120,6 +195,7 @@ void boy::ShowYourFriendInfo(Girl &frn)
 	cout << "Her phone number : " << frn.phNum << endl;
 }
 */
+
 //friend 선언 - 함수 인 경우
 /*
 class point;
@@ -190,6 +266,7 @@ public:
 	}
 };
 */
+
 /*const 객체
 class SoSimple
 {
@@ -209,8 +286,83 @@ public:
 	}
 };
 */
+
 int main(void)
 {
+	// 자료형이 다른 경우의 연산자 오버로딩
+	/**/
+	Point pos(1, 2);
+	Point cpy;
+
+	cpy = pos * 3;
+	cpy.ShowPoistion();
+
+	//연산자 오버로딩
+	/*	- 연산자가 오버로딩 되면, 피연산자의 종류에 따라서 연산의 방식이 
+		달라지는 것을 말함.
+	*/
+	// 이항 연산자
+	/*
+	Point pos1(3, 4);
+	Point pos2(10, 10);
+	
+	//연산자 오버로딩
+	//- 멤버함수에 의한 오버로딩
+	//- 전역함수에 의한 오버로딩
+	//Point pos3 = pos1 + pos2;
+	//Point pos3 = pos1.operator+(pos2);
+	//컴파일러가 pos1+pos2 를 pos1.operator+(pos2)로 해석해서 컴파일.
+	
+	Point pos3 = pos1 + pos2;
+
+	pos1.ShowPoistion();
+	pos2.ShowPoistion();
+	pos3.ShowPoistion();
+	*/
+
+	// 단한 연산자 오버로딩
+	/*
+	Point pos(1, 2);
+	Point cpy;
+	//num++ 후위 출력 후 num값 증가
+	//++num 전위 출력 전 num 값 증가
+	*/
+	//(전위 연산자)
+	/*
+	//++pos;
+	//pos.operator++();		//멤버함수의 경우
+	//operator++(pos);		//전역함수의 경우
+	pos.ShowPoistion();
+	--pos;
+	//pos.operator--();		//멤버함수의 경우
+	//operator--(pos);		//전역함수의 경우
+	pos.ShowPoistion();
+
+	++(++pos);
+	pos.ShowPoistion();
+	--(--pos);
+	pos.ShowPoistion();
+	*/
+
+	//후위 연산자
+	/*
+	//pos.operator--();		전위
+	//pos.operator++();		전위
+	//pos.operator--(int);  후위
+	//pos.operator++(int);  후위
+	/*
+	
+	cpy = pos--;
+	//pos.operator--();		//멤버함수의 경우
+	//operator--(pos);		//전역함수의 경우
+	pos.ShowPoistion();
+	cpy.ShowPoistion();
+
+	cpy = (pos++);
+	pos.ShowPoistion();
+	cpy.ShowPoistion();
+	*/
+
 	//C언어 정적 지역 변수
 	/*
 	for (int i = 0; i < 10; i++)
@@ -228,9 +380,10 @@ int main(void)
 	SoComplx cmx3;
 	*/
 	//const static 변수 (클래스 변수)
+	/*
 	cout << "러시아 면적 : " << CountryArea::RUSSIA << endl;
 	cout << "한국	면적 : " << CountryArea::SOUTH_KOREA << endl;
-
+	*/
 	//static 함수 (클래스 함수)
 	/*	
 		- 선언된 클래스의 모든 객체가 공유
@@ -240,12 +393,11 @@ int main(void)
 
 	// mutable 키워드
 	/*	- const 함수 내에서 멤버변수 값의 변경을 에외적으로 허용.
-	*/
 	SoSimple sm(1, 2);
 	sm.ShowSimpleData();
 	sm.CopyToNum2();
 	sm.ShowSimpleData();
-
+	*/
 	//friend 선언 - class 인 경우
 	/*	- friend 선언은 객체지향의 캡술화에서 정보은닉을 위배하는 문법임.
 		- 따라서 friend 선언은 필요한 상황에서만 사용해야함.
@@ -288,5 +440,6 @@ int main(void)
 	constObj.ShowData();
 	//constObj.AddNum(10);
 	*/
+
 	return 0;
 }
